605f43f95acb3214fc24dbb0f11af9d8b8bf84c0
Kelly J Andrews <kelly@kellyjandrews.com>
2020-02-04 08:25:13 -0500

    adjust for all route types

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-'use strict';
 require('dotenv').config();
 
 const express = require('express');
@@ -12,6 +11,8 @@ const server = http.createServer(app);
 const wss = new WebSocket.Server({ server, path: "/socket" });
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 let translateTo = 'en';
 
 wss.on('connection', (ws) => {
@@ -41,7 +42,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
-app.post('/inboundSMS', async function (req, res) {
+const _handleSMS = async (req, res) => {
   let translation = await translateText(req.body);
   let response = {
     from: obfuscateNumber(req.body.msisdn),
@@ -50,9 +51,14 @@ app.post('/inboundSMS', async function (req, res) {
     originalMessage: req.body.text,
     translatedTo: translateTo
   }
-  wss.clients.forEach(client => { client.send(JSON.stringify(response))});
+  wss.clients.forEach(client => { client.send(JSON.stringify(response)) });
   res.send('200');
-});
+};
+
+app.route('/inboundSMS')
+  .get(_handleSMS)
+  .post(_handleSMS)
+  .all((req, res) => res.status(405).send());
 
 setInterval(() => {
   wss.clients.forEach((ws) => {
