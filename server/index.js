cc356a3aaafe00d3f45aadbe2c6710c2038970d9
Kelly J Andrews <kelly@kellyjandrews.com>
2020-02-20 14:13:28 -0500

    correct issue with google creds

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ let config = null;
 
 if (process.env.GOOGLE_APPLICATION_CREDENTIALS === undefined) {
   config = {
-    projectId: 'nexmo-extend',
+    projectId: process.env.GOOGLE_PROJECT_ID,
     credentials: {
       client_email: process.env.GOOGLE_CLIENT_EMAIL,
       private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n')
